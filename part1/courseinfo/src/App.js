8f735e713d3f72724aae74dfcb16393a9ecfa1d5
jasonwu224 <jasonwu2688@gmail.com>
2023-02-05 01:40:03 -0500

    commit

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,63 @@
+const Part = (props) => {
+  return (
+    <p>
+      {props.part.name} {props.part.exercise}
+    </p>
+  )
+}
+
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course.name}</h1>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.course.parts[0]} />
+      <Part part={props.course.parts[1]} />
+      <Part part={props.course.parts[2]} />
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>Number of exercises {props.course.parts.reduce((total, current) => total + current.exercises, 0)}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content course={course} />
+      <Total course={course} />
+    </div>
+  )
+}
+
+export default App
